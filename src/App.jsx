07cf61804894bca40c27a4f068167893119f40da
nixx222 <167340686+nixx222@users.noreply.github.com>
2024-10-22 12:39:45 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
+
